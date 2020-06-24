28512c680f719434ac206f3e3db8e90fd3b22de4
RethimaReddy Polam <rethima.wf37@gmail.com>
2020-06-23 22:45:56 -0500

    Updated the database connectivity files

--- Code/back-end/OnlineMedico/database/migrations/1592946887604_address_schema.js
+++ Code/back-end/OnlineMedico/database/migrations/1592946887604_address_schema.js
@@ -1,24 +1,24 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class AddressSchema extends Schema {
-  up () {
-    this.create('addresses', (table) => {
-      table.increments()
-      table.string('street').notNullable()
-      table.string('city').notNullable()
-      table.string('state').notNullable()
-      table.string('country').notNullable()
-      table.integer('zip', 60).notNullable()
-      table.timestamps()
-    })
+  up() {
+    this.create("addresses", (table) => {
+      table.increments();
+      table.string("street").notNullable();
+      table.string("city").notNullable();
+      table.string("state").notNullable();
+      table.string("country").notNullable();
+      table.integer("zip", 60).notNullable();
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('addresses')
+  down() {
+    this.drop("addresses");
   }
 }
 
-module.exports = AddressSchema
+module.exports = AddressSchema;
