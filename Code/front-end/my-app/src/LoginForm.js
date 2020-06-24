ee0581f766ceb535384e340398db5e805be7da13
thumula-akhitha <akhitha.thumula@gmail.com>
2020-06-23 23:09:13 -0500

    added login css and functionality

--- Code/front-end/my-app/src/LoginForm.js
+++ Code/front-end/my-app/src/LoginForm.js
@@ -1,42 +1,53 @@
 import React from 'react';
-import axios from 'axios'; 
+import axios from 'axios';
+import logo from './images/medicine.png';
+import './css/Login.css';
 class LoginForm extends React.Component {
   render() {
     return (
-      <div className="loginForm">
-        <form onSubmit={(e) => login (e)}>
-          <h3>Sign In</h3>
-          <div className="form-group">
-            <label>Email</label>
-            <input type="email" className="form-control" id="email" placeholder="Enter your email address" aria-describedby="EmailHelp" />
+      <div className="container">
+        <div className="row">
+          <div className="col-1 logoimg">
+            <div className="logo">
+              <img src={logo} alt="logo"/>
+              <h3>OnlineMedico</h3>
+            </div>
+            </div>
+            <div className="col-11">
+            <div className="loginForm">
+              <form onSubmit={(e) => login(e)}>
+                <h3>Sign In</h3>
+                <div className="form-group">
+                  <label>Email</label>
+                  <input type="email" className="form-control" id="email" placeholder="Enter your email address" aria-describedby="EmailHelp" />
+                </div>
+                <div className="form-group">
+                  <label>Password</label>
+                  <input type="password" className="form-control" placeholder="Enter your password" id="password" />
+                </div>
+                <button type="submit" className="btn btn-info">Login</button>
+              </form>
+            </div>
+            </div>
           </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input type="password" className="form-control" placeholder="Enter your password" id="password" />
-          </div>
-          <button type="submit" className="btn btn-info">Login</button>
-        </form>
-       
-      </div>
+        </div>
     );
   }
 }
-function login(e){
+function login(e) {
   e.preventDefault();
   let request = {
     email: document.getElementById('email').value,
-    password: document.getElementById('password').value 
+    password: document.getElementById('password').value
 
   }
-  console.log(request.email)
-  console.log(request.password)
-  axios.post('http://localhost:3333/onlinemedico/login',request)
-  .then(res => {
-    alert(res.data.data);
-  })
-  .catch( err => {
-    console.log(err);
-  })
+  axios.get('http://192.168.0.10:3333/login', request)
+    .then(res => {
+      alert(res.data.message);
+    })
+    .catch(err => {
+      console.log(err);
+    })
 }
 
 export default LoginForm;
