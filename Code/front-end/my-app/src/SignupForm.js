a6bd53718d8cc8e9909ee2c5dc84469b2a1512be
Sravani537520 <sravani.jaidi98@gmail.com>
2020-06-25 13:57:15 -0500

    modified UI of signUp form

--- Code/front-end/my-app/src/SignupForm.js
+++ Code/front-end/my-app/src/SignupForm.js
@@ -1,206 +1,144 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import "react-datepicker/dist/react-datepicker.css";
+
 import axios from 'axios';
 import './css/Signup.css';
 
-function ValidationMessage(props) {
-    if (!props.valid) {
-      return(
-        <div className='error-msg'>{props.message}</div>
-      )
-    }
-    return null;
-  }
+
+
+const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+const validateForm = (errors) => {
+  let valid = true;
+  Object.values(errors).forEach(
+    (val) => val.length > 0 && (valid = false)
+  );
+  return valid;
+}
 
 class SignupForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            updatable: false,
-            firstname: '',firstnameValid:false,
-            lastname: '',lastnameValid:false,
-            password: '',passwordValid:false,
-            email: '',emailValid: false,
-            contact_number: '',contactValid:false,
-            dob: new Date(),
-            passwordConfirm: '', passwordConfirmValid: false,
-            street: '',streetValid:false,
-            state: '',stateValid:false,
-            city: '',cityValid:false,
-            zip: '',zipValid:false,
-            country:'',countryValid:false,
-            formValid: false,
-            errorMsg: {}
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: null,
+      email: null,
+      password: null,
+      confirmpwd:null,
+      contactnum:null,
+      errors: {
+        fullName: '',
+        email: '',
+        password: '',
+        confirmpwd:'',
+        contactnum:''
+      }
+    };
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let errors = this.state.errors;
+
+    switch (name) {
+      case 'fullName': 
+        errors.fullName = 
+          value.length < 5
+            ? 'Full Name must be 5 characters long!'
+            : '';
+        break;
+      case 'email': 
+        errors.email = 
+          validEmailRegex.test(value)
+            ? ''
+            : 'Email is not valid!';
+        break;
+      case 'password': 
+        errors.password = 
+          value.length < 6
+            ? 'Password must be 6 characters long!'
+            : '';
+        break;
+        case 'confirmpwd':
+                errors.confirmpwd = 
+            this.state.password !== this.state.confirmpwd 
+            ? 'does not match':'';
+           
+            break;
+      default:
+        break;
     }
-    validateForm = () => {
-        const {firstnameValid,lastnameValid, emailValid, passwordValid, passwordConfirmValid,contactValid,streetValid,stateValid,cityValid,countryValid,zipValid} = this.state;
-        this.setState({
-          formValid: firstnameValid && lastnameValid && emailValid && passwordValid && passwordConfirmValid && contactValid && streetValid && stateValid && cityValid && countryValid && zipValid
-        })
-      }
-    
-      updateFirstname = (firstname) => {
-        this.setState({firstname}, this.validateFirstname)
-      }
-    
-      validateFirstname = () => {
-        const {firstname} = this.state;
-        let firstnameValid = true;
-        let errorMsg = {...this.state.errorMsg}
-    
-        if (firstname.length < 3) {
-          firstnameValid = false;
-          errorMsg.firstname = 'Must be at least 3 characters long'
-        }
-    
-        this.setState({firstnameValid, errorMsg}, this.validateForm)
-      }
 
-      updateLastname = (lastname) => {
-        this.setState({lastname}, this.validateLastname)
-      }
-    
-      validateLastname = () => {
-        const {lastname} = this.state;
-        let lastnameValid = true;
-        let errorMsg = {...this.state.errorMsg}
-    
-        if (lastname.length < 3) {
-          lastnameValid = false;
-          errorMsg.lastname = 'Must be at least 3 characters long'
-        }
-    
-        this.setState({lastnameValid, errorMsg}, this.validateForm)
-      }
+    this.setState({errors, [name]: value});
+  }
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if(validateForm(this.state.errors)) {
+      console.info('Valid Form')
+    }else{
+      console.error('Invalid Form')
+    }
+  }
 
-    
-      updateEmail = (email) => {
-        this.setState({email}, this.validateEmail)
-      }
-    
-      validateEmail = () => {
-        const {email} = this.state;
-        let emailValid = true;
-        let errorMsg = {...this.state.errorMsg}
-    
-        // checks for format _@_._
-        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
-          emailValid = false;
-          errorMsg.email = 'Invalid email format'
-        }
-    
-        this.setState({emailValid, errorMsg}, this.validateForm)
-      }
-    
-      updatePassword = (password) => {
-        this.setState({password}, this.validatePassword);
-      }
-    
-      validatePassword = () => {
-        const {password} = this.state;
-        let passwordValid = true;
-        let errorMsg = {...this.state.errorMsg}
-    
-        // must be 6 chars
-        // must contain a number
-        // must contain a special character
-    
-        if (password.length < 6) {
-          passwordValid = false;
-          errorMsg.password = 'Password must be at least 6 characters long';
-        } else if (!/\d/.test(password)){
-          passwordValid = false;
-          errorMsg.password = 'Password must contain a digit';
-        } else if (!/[!@#$%^&*]/.test(password)){
-          passwordValid = false;
-          errorMsg.password = 'Password must contain special character: !@#$%^&*';
-        }
-    
-        this.setState({passwordValid, errorMsg}, this.validateForm);
-      }
-    
-      updatePasswordConfirm = (passwordConfirm) => {
-        this.setState({passwordConfirm}, this.validatePasswordConfirm)
-      }
-    
-      validatePasswordConfirm = () => {
-        const {passwordConfirm, password} = this.state;
-        let passwordConfirmValid = true;
-        let errorMsg = {...this.state.errorMsg}
-    
-        if (password !== passwordConfirm) {
-          passwordConfirmValid = false;
-          errorMsg.passwordConfirm = 'Passwords do not match'
-        }
-    
-        this.setState({passwordConfirmValid, errorMsg}, this.validateForm);
-      }
-      updateCountry = (country) => {
-        this.setState({country}, this.validateCountry)
-      }
-    
-      validateCountry = () => {
-        const {country} = this.state;
-        let countryValid = true;
-        let errorMsg = {...this.state.errorMsg}
-    
-        if (country.length < 3) {
-          countryValid = false;
-          errorMsg.country = 'Must be at least 3 characters long'
-        }
-    
-        this.setState({countryValid, errorMsg}, this.validateForm)
-      }
-    render() {
-        return (
-            <div class="signup-form">
-
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"/>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-            <form action="" method="post">
-                <h2>Register</h2>
-                <p class="hint-text">Create your account. It's free and only takes a minute.</p>
-                <div class="form-group">
-                    <div class="row">
-                    < ValidationMessage valid={this.state.firstnameValid} message={this.state.errorMsg.firstname} />
-                        <div class="col"><input type="text" class="form-control" name="firstname" placeholder="First Name" required="required"/>
-                        {/* value={this.state.firstname} */}
-                         {/* onChange={(e) => this.updateFirstname(e.target.value)}  */}
-                         </div>
-                        <div class="col"><input type="text" class="form-control" name="last_name" placeholder="Last Name" required="required"/></div>
-                    </div>        	
-                </div>
-                <div class="form-group">
-                    <input type="email" class="form-control" name="email" placeholder="Email" required="required"/>
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" name="password" placeholder="Password" required="required"/>
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required"/>
-                </div>        
-                <div class="form-group">
-                    <label class="form-check-label"><input type="checkbox" required="required"/> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
-                </div>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-success btn-lg btn-block">Register Now</button>
-                </div>
-                
-            </form>
-            <div class="text-center">Already have an account? <Link className="links" to="/Login">SignIn</Link></div>
+  render() {
+    const {errors} = this.state;
+    return (
+      <div className='wrapper'>
+        <div className='form-wrapper'>
+          <h2>Create Account</h2>
+          <form onSubmit={this.handleSubmit} noValidate>
+            <div className='fullName'>
+              <label htmlFor="fullName">Full Name</label>
+              <input type='text' name='fullName' onChange={this.handleChange} noValidate />
+              {errors.fullName.length > 0 && 
+                <span className='error'>{errors.fullName}</span>}
+            </div>
+            <div className='email'>
+              <label htmlFor="email">Email</label>
+              <input type='email' name='email' onChange={this.handleChange} noValidate />
+              {errors.email.length > 0 && 
+                <span className='error'>{errors.email}</span>}
+            </div>
+            <div className='password'>
+              <label htmlFor="password">Password</label>
+              <input type='password' name='password' onChange={this.handleChange} noValidate />
+              {errors.password.length > 0 && 
+                <span className='error'>{errors.password}</span>}
+            </div>
+            <div className='confirmpassword'>
+              <label htmlFor="confirmpassword"> Retype Password</label>
+              <input type='confirmpassword' name='confirmpassword' onChange={this.handleChange} noValidate />
+              {errors.confirmpwd.length > 0 && 
+                <span className='error'>{errors.confirmpwd}</span>}
+            </div>
+            <div className='contactnum'>
+              <label htmlFor="contactnum">Contact Number</label>
+              <input type='text' name='contactnum' onChange={this.handleChange} noValidate />
+              {errors.contactnum.length > 0 && 
+                <span className='error'>{errors.contactnum}</span>}
+            </div>
+           
+            <label class="form-check-label">
+                <div>
+              <input type="checkbox" className='checkbox' required="required" /> 
+              <p>I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></p>
+              </div>
+              </label>
+            <div className='submit'>
+              <button>Create</button>
+              <div className='info'>
+              <p>Already Have an account<Link className="links" to="Login">Sign In</Link></p>
+            </div>
+            </div>
+          </form>
         </div>
-        
-       
-        );
-    }
+      </div>
+    );
+  }
 }
+
+
+
 function signup(e) {
     e.preventDefault();
     let request = {
