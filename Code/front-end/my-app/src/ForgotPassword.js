cc0eab7111f135a47021a7bf96c57aa102e1bb4e
kavyareddy536886 <54380608+kavyareddy536886@users.noreply.github.com>
2020-06-24 21:47:18 -0400

    updated forgot password code

--- Code/front-end/my-app/src/ForgotPassword.js
+++ Code/front-end/my-app/src/ForgotPassword.js
@@ -1,15 +1,19 @@
 
 import React, { Component } from 'react'
+
 import { Text, View } from 'react-native'
 
 class ForgotPassword extends Component {
   render() {
     return (
+
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Forgot Password </Text>
       </View>
+
     )
   }
 }
+
 
 export default ForgotPassword
