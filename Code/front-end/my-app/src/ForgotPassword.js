3d6248c527945ddcd6effcddd31bd386b0dd1bec
Sravani537520 <sravani.jaidi98@gmail.com>
2020-07-01 00:21:50 -0500

    changed styling of forgotpassword page

--- Code/front-end/my-app/src/ForgotPassword.js
+++ Code/front-end/my-app/src/ForgotPassword.js
@@ -10,7 +10,7 @@ class ForgotPassword extends React.Component{
     e.preventDefault();
   let request = {
    
-    email: document.getElementById('email').value
+    email: document.getElementById('mail').value
 
     }
     axios.post('http://127.0.0.1:3333/onlinemedico/forgotpassword', request)
@@ -41,7 +41,7 @@ render()
         <div className="forgot">
         <h4>Password Assistance</h4>
         <label htmlFor="email"> Give your reset link mail: </label>
-        <input type="email" className="form-control" id="email" placeholder="Enter your email address" aria-describedby="EmailHelp" required/>
+        <input type="email" className="form-control" id="mail" placeholder="Enter your email address" aria-describedby="EmailHelp" required/>
         <br />
 
         <button type="submit" className="send-btn btn-info">Send Link</button>
