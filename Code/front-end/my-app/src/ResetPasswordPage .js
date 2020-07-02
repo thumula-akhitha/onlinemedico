d31158ce7f140d71efb35c04a84053382a273eea
rohan6471 <54380500+rohan6471@users.noreply.github.com>
2020-07-01 22:41:20 -0500

    modified reset password page

--- Code/front-end/my-app/src/ResetPasswordPage .js
+++ Code/front-end/my-app/src/ResetPasswordPage .js
@@ -2,7 +2,6 @@ import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './css/ResetPassword.css';
-import { resetWarningCache } from 'prop-types';
 class ResetPasswordPage extends React.Component {
   constructor(props){
     super(props)
