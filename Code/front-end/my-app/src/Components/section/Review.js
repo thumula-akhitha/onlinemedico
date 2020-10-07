0d489335ba9853692b75cfaa01f275975ec799bd
sravani537520 <sravani.jaidi98@gmail.com>
2020-10-06 20:00:56 -0500

    added product review page front end code

---
+++ Code/front-end/my-app/src/Components/section/Review.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import '../css/Review.css';
+import { Card } from 'react-bootstrap';
+import StarComponent from './StarComponent';
+import Profile from '../images/Profile.PNG';
+import axios from "axios";
+
+class Review extends Component {
+    constructor() {
+        super();
+        this.review = this.review.bind(this);
+
+        }
+        review(e) {
+            e.preventDefault();
+            let request = {
+                headline: document.getElementById("headline").value,
+                likeDislike: document.getElementById("likeDislike").value,
+                reviewName: document.getElementById("reviewName").value,
+            };
+            axios
+              .post("http://127.0.0.1:3333/onlinemedico/user/userReview", request)
+              .then((res) => {
+                  console.log(res.data)
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+            }
+    render() {
+        return (
+            <form >
+                <div className='reviewPage'>
+                    <p className='reviewTitle'>Create Review</p>
+                    <div className='imageCard'>
+                        <div className='row'>
+                            <div className='col-5'>
+                                {this.props.orderedArray.map(
+                                    orderimage => { return (<img src={orderimage.image} width='131px' height='100px' />) }
+                                )}
+                            </div>
+                            <div className='col-7'>
+                                {this.props.orderedArray[0].title}
+                              
+                            </div>
+                        </div>
+                    </div>
+                    <div className='otherFeatures'>
+                        <div className='overallReview'>
+                            <p className='overall'>Overall rating</p>
+                            <StarComponent />
+                        </div>
+                        <p className='features'>Rate features</p>
+                       
+                        <div className='qualityReview'>
+                            <p className='quality'>Quality</p>
+                            <StarComponent />
+                        </div>
+                        <div className='logisticReview'>
+                            <p className='logistic'>Logistic Service</p>
+                            <StarComponent />
+                        </div>
+                        <div className='addlineReview'>
+                            <p className='reviewHeadline'>Add headline</p>
+                            <input type='text' className='headline' id='headline' placeholder='most important to know' />
+
+                            <p className='reviewText'>Write your Review</p>
+                            <textarea className='dislike' id='likeDislike' placeholder='like or dislike about'></textarea>
+                            <p className='publicName'>Choose your public Name</p>
+                            <img className='profile' src={Profile}></img>
+                            <input className='reviewName' id='reviewName' onChange={this.handleInputChange}/>
+                            <span className='errors'>{this.state.errors.publicName}</span>
+                            <p className='nameChange'>You can change this name anytime in the profile</p>
+                        </div>
+                        <button className='reviewSubmit' onClick={(e)=>e.preventDefault()}>Submit</button>
+                    </div>
+                </div>
+            </form>
+        )
+
+    }
+
+}
+export default Review;
